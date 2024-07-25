00f5e164e1191c887014a8337fd7c8556908be1d
akiya-7 <jaxonakhyar@gmail.com>
2024-07-25 11:58:54 +1000

    Feat: Completed step 7 of courseinfo (exercise 2)

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,8 +1,12 @@
 const Course = ({course}) => {
+    const exercises = course.parts.map(exercise => exercise.exercises)
+    const totalExercises = exercises.reduce((add, curr) => add + curr, 0);
+
     return(
             <>
                 <h1>{course.name}</h1>
                 {course.parts.map(part => <p key={part.id}>{part.name} {part.exercises}</p>)}
+                <p><strong>total of {totalExercises} exercises</strong></p>
             </>
     )
 }
