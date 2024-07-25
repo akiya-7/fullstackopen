4a9a3e51b4ef0f54e865315b17a0195bdbaac702
akiya-7 <jaxonakhyar@gmail.com>
2024-07-25 12:35:06 +1000

    Feat: Completed step 8-10 of courseinfo (exercise 3-5)

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,13 +1,22 @@
-const Course = ({course}) => {
-    const exercises = course.parts.map(exercise => exercise.exercises)
-    const totalExercises = exercises.reduce((add, curr) => add + curr, 0);
-
+const Course = ({courses}) => {
     return(
-            <>
-                <h1>{course.name}</h1>
-                {course.parts.map(part => <p key={part.id}>{part.name} {part.exercises}</p>)}
-                <p><strong>total of {totalExercises} exercises</strong></p>
-            </>
+        <>
+        {courses.map(course => {
+            const { name, parts } = course;
+            const exercises = parts.map(part => part.exercises)
+            const totalExercises = exercises.reduce((add, curr) => add + curr, 0);
+
+                return (
+                    <div key={course.id}>
+                        <h1>{name}</h1>
+                        {parts.map(part => (
+                            <p key={part.id}>{part.name} {part.exercises}</p>
+                        ))}
+                        <p><strong>total of {totalExercises} exercises</strong></p>
+                    </div>
+                )
+            })}
+        </>
     )
 }
 
