8844591d0d403d347ec1c26664bb8703f65b35e2
akiya-7 <jaxonakhyar@gmail.com>
2024-07-27 13:26:54 +1000

    Feat: Completed step 6 of phonebook (exercise 1)

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,11 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 import Filter from './components/Filter'
+import axios from "axios";
 
 const App = () => {
-    const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+
+    const [persons, setPersons] = useState([])
     const [filter, setFilter] = useState('')
+
+    useEffect(() => {
+        console.log('effect')
+        axios
+            .get('http://localhost:3001/persons')
+            .then(res => {
+                console.log('promise fulfilled')
+                setPersons(res.data)
+            })
+    }, [])
+    console.log('render', persons.length, 'people')
 
     const handleNewFilter = (filter) => {
         setFilter(filter)
@@ -14,13 +27,14 @@ const App = () => {
         const personsCopy = [...persons]
         const names = personsCopy.map((person) => person.name)
 
-        console.log(names,newPerson.name,)
         if (names.includes(newPerson.name)) {
             alert(`${newPerson.name} is already added to phonebook`)
         } else {
+            newPerson.id = (personsCopy.length + 1).toString()
             personsCopy.push(newPerson)
             setPersons(personsCopy)
-            console.log('name', newPerson.name, 'with phone number', newPerson.phoneNumber)
+            console.log('name', newPerson.name, 'with phone number', newPerson.number)
+            console.log(personsCopy)
         }
     }
 
