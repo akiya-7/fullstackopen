e90cd54c47944a369d79c059f9cb34a6bcd878dc
akiya-7 <jaxonakhyar@gmail.com>
2024-07-29 13:58:18 +1000

    Feat: Completed step 11 of phonebook (exercise 16)

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,33 +2,49 @@ import { useState, useEffect } from 'react'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 import Filter from './components/Filter'
+import Alert from './components/Alert'
 import personsService from './services/persons.js';
 
 const App = () => {
 
     const [persons, setPersons] = useState([])
     const [filter, setFilter] = useState('')
+    const [alertMessage, setAlertMessage] = useState('')
+    const [alertType, setAlertType] = useState('')
 
     console.log('render', persons.length, 'people')
+
+    const displayAlert = (alertMessage, alertType) => {
+        setAlertMessage(alertMessage)
+        setAlertType(alertType)
+        setTimeout(() => {
+            setAlertMessage(null)
+        }, 5000)
+    };
+
     useEffect(() => {
-        console.log('effect')
+        console.log('getting initial data...')
         personsService
             .getAll()
             .then(people => {
                 setPersons(people)})
     }, [])
-
     const handleNewFilter = (filter) => {
         setFilter(filter)
     }
     const handleNewPerson = (newPerson) => {
+        const errorMessage = ``
+
         personsService
             .create(newPerson)
+            .catch(error => displayAlert(errorMessage, 'error'))
             .then(newPerson => {
                 setPersons(persons.concat(newPerson))}
             )
+        const alertMessage= `name ${newPerson.name} with phone number ${newPerson.number}`
+        displayAlert(alertMessage, "success")
+
         console.log('name', newPerson.name, 'with phone number', newPerson.number)
-        console.log(persons.concat(newPerson))
     }
     const handleNumberUpdate = (newPerson) => {
         personsService
@@ -67,10 +83,10 @@ const App = () => {
         }
     }
 
-
     return (
         <div>
             <h2>Phonebook</h2>
+            <Alert message={alertMessage} type={alertType}/>
             <Filter persons={persons} onFilterChange={handleNewFilter} />
             <h3>Add a new... </h3>
             <PersonForm persons={persons} onPersonSubmit={handlePersonSubmit} />
