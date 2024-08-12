74a4a5856582ef2b6d9dcc262a9543a30c8832a4
akiya-7 <jaxonakhyar@gmail.com>
2024-08-12 13:48:41 +1000

    Feat: Completed Step 2 of "Helper Functions and Unit Tests" (exercise 4)

--- part4/blog/utils/list_helpers.js
+++ part4/blog/utils/list_helpers.js
@@ -2,4 +2,16 @@ const dummy = (blogs) => {
   return 1;
 }
 
-module.exports = {dummy}
+const totalLikes = (blogs) => {
+  let totalLikes = 0;
+
+  if (blogs){
+    const likes = blogs.map(blog => blog.likes)
+    totalLikes = likes.reduce((total, like) => total + like, 0)
+  }
+
+  return totalLikes
+
+}
+
+module.exports = {dummy, totalLikes}
