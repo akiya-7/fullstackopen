e5012e2a633dc9e3b402d26c11fa3fc7af614a44
akiya-7 <jaxonakhyar@gmail.com>
2024-08-12 14:11:56 +1000

    Feat: Completed Step 3 of "Helper Functions and Unit Tests" (exercise 5)

--- part4/blog/utils/list_helpers.js
+++ part4/blog/utils/list_helpers.js
@@ -14,4 +14,23 @@ const totalLikes = (blogs) => {
 
 }
 
-module.exports = {dummy, totalLikes}
+const favouriteBlog = (blogs) => {
+  if(blogs.length === 0)
+    return {};
+
+  let favouriteIndex = 0;
+  const blogsLikes = blogs.map(blog => blog.likes)
+
+  for (let i = 0; i < blogsLikes.length; i++) {
+    if (blogsLikes[i] > blogsLikes[favouriteIndex])
+      favouriteIndex = i
+    }
+
+  return {
+    title: blogs[favouriteIndex].title,
+    author: blogs[favouriteIndex].author,
+    likes: blogs[favouriteIndex].likes,
+  }
+}
+
+module.exports = {dummy, totalLikes, favouriteBlog}
