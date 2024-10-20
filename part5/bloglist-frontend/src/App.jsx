dbfe7b116aad3891a1d964a4d409403a67dc0805
jaxonakhyar@gmail.com <jaxonakhyar@gmail.com>
2024-10-20 18:14:59 +1100

    Completed: Exercise 5.3 - Logged in users can now add new blogs

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import Login from './components/Login'
+import AddBlog from './components/AddBlog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
@@ -19,6 +20,7 @@ const App = () => {
     if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON)
       setUser(user)
+      blogService.setToken(user.token)
     }
   }, [])
 
@@ -32,6 +34,7 @@ const App = () => {
         'loggedUser', JSON.stringify(user)
       )
     setUser(user)
+    blogService.setToken(user.token)
   }
 
   const handleLogout = () => {
@@ -42,12 +45,19 @@ const App = () => {
   if (user === null)
     return(<Login onLogin={handleLogin}/>)
 
+  const handleNewBlog = async (newBlog) => {
+    const response = await blogService.postBlog(newBlog, { headers: { Authorization: `Bearer ${user}`}})
+    blogService.getAll().then(blogs => setBlogs( blogs ))
+  }
+
   return (
       <div>
         <h2>blogs</h2>
         <p>
           Hello {user.name}! <button id="logout" onClick={() => {handleLogout()}}>Logout</button>
         </p>
+
+        <AddBlog onNewBlog={handleNewBlog}/>
 
         {blogs.map(blog =>
             <Blog key={blog.id} blog={blog}/>
