b9f8950f6a40036d856517bd280870e69d17a97e
jax ! <jaxonakhyar@gmail.com>
2024-11-03 16:28:35 +1100

    Completed: Exercise 6.14 - anecdotes are fetched from json-server

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,9 +1,18 @@
-import AnecdoteForm from './components/AnecdoteForm';
+import {useDispatch} from 'react-redux';
+import {useEffect} from 'react';
 import AnecdoteList from './components/AnecdoteList';
 import Filter from './components/Filter';
 import Notification from './components/Notification.jsx';
+import AnecdoteForm from './components/AnecdoteForm';
+import anecdoteService from './services/anecdotes';
+import {setAnecdotes} from './reducers/anecdoteReducer.js';
+
 
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    anecdoteService.getAll().then(anecdotes => dispatch(setAnecdotes(anecdotes)));
+  }, [])
 
   return (
     <div>
