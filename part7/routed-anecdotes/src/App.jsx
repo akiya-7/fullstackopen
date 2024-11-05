e7424126226f848cc94c275f20dc93e8d87e198d
jax ! <jaxonakhyar@gmail.com>
2024-11-05 16:42:01 +1100

    Completed: Exercise 7.4 - useField hook now used for form

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -7,6 +7,7 @@ import {
 } from 'react-router-dom';
 import Anecdote from '../src/components/Anecdote.jsx'
 import Notification from './components/Notification.jsx';
+import {useField} from './hooks';
 
 const Menu = () => {
   const padding = {
@@ -55,17 +56,17 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
 
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
   }
@@ -76,15 +77,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input name='content' {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input name='author' {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input name='info' {...info} />
         </div>
         <button>create</button>
       </form>
@@ -127,7 +128,8 @@ const App = () => {
   const addNew = (anecdote) => {
     anecdote.id = Math.round(Math.random() * 10000)
     setAnecdotes(anecdotes.concat(anecdote))
-    displayNotification(`a new anecdote ${anecdote.content} created!`)
+    console.log(anecdote)
+    displayNotification(`a new anecdote ${anecdote.content.value} created!`)
   }
 
   const vote = (id) => {
