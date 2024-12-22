1be13e405dfc242ef99fabb4936862243ad52f03
jax ! <jaxonakhyar@gmail.com>
2024-12-22 12:20:48 +1100

    Complete: Exercise 9.3 - Allow execution of scripts through console.

--- part9/bmiCalculator.ts
+++ part9/bmiCalculator.ts
@@ -1,3 +1,26 @@
+interface bodyInfo {
+    height: number;
+    weight: number;
+}
+
+const validateInput = (args: string[]): bodyInfo => {
+    if (args.length != 4) {
+        throw new Error("Incorrect number of arguments.")
+    }
+        const height = Number(args[2]);
+        const weight = Number(args[3]);
+
+        if (isNaN(height)) {
+            throw new Error("Your height parameter is invalid.");
+        }
+
+        if (isNaN(weight)) {
+            throw new Error("Your weight parameter is invalid.");
+        }
+
+        return {height, weight};
+};
+
 const calculateBmi = (height: number, weight: number) => {
     const bmi: number = weight / ((height/100) ** 2);
 
@@ -12,4 +35,10 @@ const calculateBmi = (height: number, weight: number) => {
     }
 }
 
-console.log(calculateBmi(180, 74))
+const main = (args: string[]): number => {
+    const {height, weight} = validateInput(args);
+    console.log(calculateBmi(height, weight));
+    return 1;
+}
+
+main(process.argv)
--- part9/exerciseCalculator.ts
+++ part9/exerciseCalculator.ts
@@ -7,6 +7,10 @@ interface Result {
     target: number,
     average: number,
 }
+interface Input {
+    workoutHours: number[];
+    targetHours: number;
+}
 
 const calculateExercises = (workoutHours: number[], targetHours: number): Result => {
     const totalHours: number = workoutHours.reduce((sum,hours) => sum += hours, 0)
@@ -15,26 +19,23 @@ const calculateExercises = (workoutHours: number[], targetHours: number): Result
     let rating: number;
     let ratingDescription: string = "";
 
-    if (averageHours < (targetHours * 0.8)) {rating = 1;}
-    else if (averageHours > (targetHours * 0.8) && averageHours < targetHours) {rating = 2;}
-    else {rating = 3;}
-
-    switch (rating){
-        case 1:
-            ratingDescription = "Your average hours was less than expected, consider adjusting your target time or reassessing your workouts.";
-            break;
-        case 2:
-            ratingDescription = "So close! Keep up the work for another cycle and I'm sure you'll reach the goal next time.";
-            break;
-        case 3:
-            ratingDescription = "Smashed it! Congratulations on hitting your target goal!";
-            break;
+    if (averageHours < (targetHours * 0.8)) {
+        rating = 1;
+        ratingDescription = "Your average hours was less than expected, consider adjusting your target time or reassessing your workouts.";
+    }
+    else if (averageHours > (targetHours * 0.8) && averageHours < targetHours) {
+        rating = 2;
+        ratingDescription = "So close! Keep up the work for another cycle and I'm sure you'll reach the goal next time.";
+    }
+    else {
+        rating = 3;
+        ratingDescription = "Smashed it! Congratulations on hitting your target goal!";
     }
 
     return {
         periodLength: workoutHours.length,
         trainingDays: workoutHours.filter((hours: number) => hours > 0).length,
-        success: averageHours > targetHours,
+        success: averageHours >= targetHours,
         rating: rating,
         ratingDescription: ratingDescription,
         target: targetHours,
@@ -42,4 +43,49 @@ const calculateExercises = (workoutHours: number[], targetHours: number): Result
     }
 }
 
-console.log(calculateExercises([0,0,0,4,1,5,21,0], 4))
+const validateInput = (args: string[]): Input => {
+    if (args.length < 4) {
+        throw new Error("Invalid input. There needs to be at least 2 numbers.")
+    }
+
+    const workoutHours: number[] = [];
+
+    for (let i = 2; i < (args.length - 1); i++) {
+        if (isNaN(Number(args[i]))) {
+            throw new Error("Invalid input. The workout hours must be numbers.")
+        }
+
+        workoutHours.push(Number(args[i]));
+    }
+
+    const targetHours = Number(args[(args.length - 1)])
+
+    if ( isNaN(targetHours) ) {
+        throw new Error("Invalid input. The target hours must be numbers.")
+    }
+
+
+    return {
+        workoutHours: workoutHours,
+        targetHours: targetHours
+    }
+}
+
+const main = (args: string[]): number => {
+    try {
+        const {targetHours, workoutHours}: Input = validateInput(args);
+        console.log(calculateExercises(workoutHours, targetHours))
+        return 0;
+    }
+    catch (e: unknown) {
+        if (e instanceof Error) {
+            console.error("Error:", e.message)
+        }
+        else {
+            console.error("An unknown error occurred.")
+        }
+        return 1;
+    }
+}
+
+main(process.argv)
